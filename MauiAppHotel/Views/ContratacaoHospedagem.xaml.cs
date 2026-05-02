c50c2fe06e06880fc77fd8cd3050a171b5daf2a5
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2026-05-02 19:31:48 -0300

    feat: Pagina Hospedagem Contratada e navegação

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,16 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			await Navigation.PushAsync(new HospedagemContratada());
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlertAsync("Ops...", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
+    }
 }
